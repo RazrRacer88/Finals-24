9d9cea34e22562e45fce6ab221697af281f4fc2b
RzrRacer88 <Georgellamosas@hotmail.com>
2024-05-08 13:36:42 -0700

    working on level

---
+++ Finals 24/Assets/Scriptss/Menus.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menus : MonoBehaviour
+{
+
+    public int levelOne;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+    public void OnPlayButtonPressed()
+    {
+        SceneManager.LoadScene(levelOne);
+    }
+
+    public void OnQuitButtonPressed()
+    {
+        Application.Quit();
+    }
+}
